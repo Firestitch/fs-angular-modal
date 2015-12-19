77638bc2cbafb891c82ee7857c51e66b1d479723
Raymond <ray@firestitch.com>
2015-12-19 12:32:56 -0500

    commit

---
+++ js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fs-angular-modal.services:fsModal",
+      "shortName": "fsModal",
+      "type": "interface",
+      "moduleName": "fs-angular-modal",
+      "shortDescription": "",
+      "keywords": "$mddialog allow api controller customization data dirertly format fs-angular-modal full function injected interface legacy method migrated modal namespace object objects open options pass passed services template url view wraps"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
