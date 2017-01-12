8dd2d0326c535158a7386700bbf2a76a64d6bca6
Raymond <ray@firestitch.com>
2017-01-12 14:15:04 -0500

    updated better promise fulfilment on cancel events

--- dist/modal.js
+++ dist/modal.js
@@ -155,96 +155,100 @@
          */
         function confirm(options) {
 
-            if(modals) {
-                return;
-            }
+            $q(function(resolve,reject) {
 
-            modals++;
+            	if(modals) {
+	                return reject();
+	            }
 
-            var defer = $q.defer();
+	            modals++;
 
-            var confirm = { template: [
-                            '<md-dialog md-theme="{{ dialog.theme }}" aria-label="{{ dialog.ariaLabel }}" class="fs-modal-confirm {{ dialog.css }}">',
-                            ' <md-dialog-content class="md-dialog-content" tabIndex="-1">',
-                            '   <h2 class="md-title">{{ dialog.title }}</h2>',
-                            '   {{dialog.mdTextContent || dialog.content}}',
-                            ' </md-dialog-content>',
-                            ' <md-dialog-actions>',
-                            '   <md-button ng-click="dialog.cancel($event)">{{dialog.cancelLabel}}</md-button>',
-                            '   <md-button ng-click="dialog.ok($event)" class="md-accent" md-autofocus="dialog.$type!=\'confirm\'">{{dialog.okLabel}}</md-button>',
-                            ' </md-dialog-actions>',
-                            '</md-dialog>'
-                            ].join('').replace(/\s\s+/g, ''),
-                            controller: function () {
+           		var confirm = {
+           			template: [
+	                    '<md-dialog md-theme="{{ dialog.theme }}" aria-label="{{ dialog.ariaLabel }}" class="fs-modal-confirm {{ dialog.css }}">',
+	                    ' <md-dialog-content class="md-dialog-content" tabIndex="-1">',
+	                    '   <h2 class="md-title">{{ dialog.title }}</h2>',
+	                    '   {{dialog.mdTextContent || dialog.content}}',
+	                    ' </md-dialog-content>',
+	                    ' <md-dialog-actions>',
+	                    '   <md-button ng-click="dialog.cancel($event)">{{dialog.cancelLabel}}</md-button>',
+	                    '   <md-button ng-click="dialog.ok($event)" class="md-accent" md-autofocus="dialog.$type!=\'confirm\'">{{dialog.okLabel}}</md-button>',
+	                    ' </md-dialog-actions>',
+	                    '</md-dialog>'
+	                    ].join(''),
+                    controller: function() {
 
-                                this.ok = function() {
-                                    var result = undefined;
+                        this.ok = function() {
+                            var result = undefined;
 
-                                    if(options.ok)
-                                        result = options.ok();
+                            if(options.ok)
+                                result = options.ok();
 
-                                    $q(function(resolve) {
-                                        // resolve() accepts promises as well and recpects them.
-                                        resolve(result ? result : true);
-                                    })
-                                    .then(function(value) {
-                                        // hide() returns promise that is resolved when the dialog has been closed.
-                                        $mdDialog.hide(value).then(function() {
-                                            defer.resolve(value);
-                                        });
-                                    })
-                                    // in case if rejected promise was retured from options.ok()
-                                    .catch(function(reason) {
-                                        $mdDialog.hide(reason).then(function() {
-                                            defer.resolve(reason);
-                                        });
-                                    });
-                                };
+                            $q(function(resolve) {
+                                // resolve() accepts promises as well and recpects them.
+                                resolve(result ? result : true);
+                            })
+                            .then(function(value) {
+                                // hide() returns promise that is resolved when the dialog has been closed.
+                                $mdDialog.hide(value).then(function() {
+                                    defer.resolve(value);
+                                });
+                            })
+                            // in case if rejected promise was retured from options.ok()
+                            .catch(function(reason) {
+                                $mdDialog.hide(reason).then(function() {
+                                    defer.resolve(reason);
+                                });
+                            });
+                        };
 
-                                this.cancel = function($event) {
-                                    var result = undefined;
+                        this.cancel = function($event) {
 
-                                    if(options.cancel)
-                                        result = options.cancel();
+                            $q(function(cancelResolve) {
+                            	var result = undefined;
 
-                                    $q(function(resolve) {
-                                        resolve(result ? result : false);
-                                    })
-                                    .then(function(value) {
-                                        $mdDialog.hide(value).then(function() {
-                                            defer.reject(value);
-                                        });
-                                    })
-                                    // in case if rejected promise was retured from options.cancel()
-                                    .catch(function(reason) {
-                                        $mdDialog.hide(reason).then(function() {
-                                            defer.reject(reason);
-                                        });
-                                    });
-                                };
-                            },
-                            preserveScope: true,
-                            controllerAs: 'dialog',
-                            bindToController: true,
-                            title: options.title || 'Confirm',
-                            content: options.content,
-                            ariaLabel: 'Confirm',
-                            focusOnOpen: options.focusOnOpen!==false,
-                            skipHide: true,
-                            locals: {
-                                okLabel: options.okLabel || 'Yes',
-                                cancelLabel: options.cancelLabel || 'Cancel'
-                            },
-                            onShowing: function($scope,container) {
-                                angular.element(container).addClass('fs-modal-confirm-container');
-                            }};
+                                if(options.cancel) {
+                                    result = options.cancel();
+                                }
 
-            $mdDialog.show(confirm).finally(function(result) {
-                modals--;
-                return result;
+                                if(angular.isObject(result) && result.then) {
+                                	result.then(cancelResolve);
+
+                                } else {
+                                	cancelResolve(result);
+                                }
+                            })
+                            .then(function(result) {
+								$mdDialog.hide(result)
+								.then(function() {
+                                    reject(result);
+                                });
+                            })
+                        }
+                    },
+                    preserveScope: true,
+                    controllerAs: 'dialog',
+                    bindToController: true,
+                    title: options.title || 'Confirm',
+                    content: options.content,
+                    ariaLabel: 'Confirm',
+                    focusOnOpen: false,
+                    skipHide: true,
+                    locals: {
+                        okLabel: options.okLabel || 'Yes',
+                        cancelLabel: options.cancelLabel || 'Cancel'
+                    },
+                    onShowing: function($scope,container) {
+                        angular.element(container).addClass('fs-modal-confirm-container');
+                    }
+                };
+
+	            $mdDialog.show(confirm).finally(function(result) {
+	                modals--;
+	                return result;
+	            });
+
             });
-
-            return defer.promise;
         }
     });
 })();
